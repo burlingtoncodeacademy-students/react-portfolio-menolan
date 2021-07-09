4b36e7b268c61e3418b8e31082218dac326ffcd1
Matt Nolan <menolan22@gmail.com>
2021-07-09 17:31:13 -0400

    general setup, continuing work on laptop

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import './App.css';
+import About from "./Components/About.js"
+import Contact from "./Components/Contact.js"
+import Hobbies from "./Components/Hobbies.js"
+import Home from "./Components/Home.js"
+import Projects from "./Components/Projects.js"
+import Work from "./Components/Work.js"
+
 
 function App() {
   return (
