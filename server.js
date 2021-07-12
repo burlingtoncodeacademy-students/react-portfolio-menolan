4434954970101b764fbff938254ed4a200835404
Matt <menolan22@gmail.com>
2021-07-12 09:36:55 -0400

    adding changes for Heroku

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+//Initializing express
 const express = require('express')
+const app = express()
+//setting up a default port
+const port = process.env.PORT || 5000
+//binding our server to a static directory
+app.use(express.static("./client/build"))
+//setting up a route handler
+app.get("*", (req, res) => {
+    res.sendFile(__dirname + "/client/build/index.html")
+})
+//open a connection to listen for and respond to requests
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+})
